8f615f91b7ff7110244343d9d8af012b2bc3f086
Dave Liddament <dave@lampbristol.com>
2017-06-07 14:24:46 +0000

    ADD PersonRepository

---
+++ src/AppBundle/Repository/PersonRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Repository;
+
+
+use AppBundle\Entity\Person;
+use AppBundle\Model\EmailAddress;
+use Doctrine\ORM\EntityManagerInterface;
+
+class PersonRepository
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * PersonRepository constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    /**
+     * Save a Person object
+     *
+     * @param Person $person
+     */
+    public function persist(Person $person)
+    {
+        $this->entityManager->persist($person);
+        $this->entityManager->flush();
+    }
+
+
+    /**
+     * Returns the person with the given email address
+     *
+     * @param EmailAddress $emailAddress
+     * @return Person|null
+     */
+    public function findByEmailAddress(EmailAddress $emailAddress)
+    {
+        $repository = $this->entityManager->getRepository(Person::class);
+        return $repository->findOneBy(['emailAddress' => $emailAddress->getEmailAddress()]);
+    }
+}
