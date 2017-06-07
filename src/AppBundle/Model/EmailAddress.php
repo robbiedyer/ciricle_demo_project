18c796f528d538b9dcd9fbefe1dc42be69b9d54b
Dave Liddament <dave@lampbristol.com>
2017-06-07 13:27:38 +0000

    ADD EmailAddress value object and some tests

---
+++ src/AppBundle/Model/EmailAddress.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Model;
+
+use Exception;
+
+
+/**
+ * Represents an email address
+ */
+class EmailAddress
+{
+
+    /**
+     * @var string
+     */
+    private $emailAddress;
+
+    /**
+     * EmailAddress constructor.
+     * @param string $rawEmailAddress
+     * @throws Exception
+     */
+    public function __construct($rawEmailAddress)
+    {
+        if (!filter_var($rawEmailAddress, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Invalid email address [$rawEmailAddress]");
+        }
+
+        $this->emailAddress = strtolower($rawEmailAddress);
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmailAddress()
+    {
+        return $this->emailAddress;
+    }
+}
---
+++ src/AppBundle/Tests/Unit/Model/EmailAddressTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Tests\Unit\Model;
+
+
+use AppBundle\Model\EmailAddress;
+use PHPUnit_Framework_TestCase;
+
+class EmailAddressTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @return array
+     */
+    public function validEmailAddressDataProvider()
+    {
+        return [
+            ['dave@example.com', 'dave@example.com'],
+            ['dave@EXAMPLE.com', 'dave@example.com'],
+        ];
+    }
+
+
+    /**
+     * @dataProvider validEmailAddressDataProvider
+     *
+     * @param string $inputEmailAddress
+     * @param string $expectedEmailAddress
+     */
+    public function testNormaliseEmailAddress($inputEmailAddress, $expectedEmailAddress)
+    {
+        // We are just constructing an EmailAddress object. If there are no exceptions we know that everything is OK
+        $emailAddress = new EmailAddress($inputEmailAddress);
+        $this->assertEquals($expectedEmailAddress, $emailAddress->getEmailAddress());
+    }
+
+
+    /**
+     * @return array
+     */
+    public function invalidEmailAddressDataProvider()
+    {
+        return [
+            ['@example.com'],
+            ['dave@example'],
+            ['dave.example.com'],
+        ];
+    }
+
+
+    /**
+     * @dataProvider invalidEmailAddressDataProvider
+     * @expectedException \Exception
+     *
+     * @param $inputEmailAddress
+     */
+    public function testInvalidEmailAddress($inputEmailAddress)
+    {
+        new EmailAddress($inputEmailAddress);
+    }
+}
